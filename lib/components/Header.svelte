f952752b83c92c846db5a816f5bbf3a99c7598c6
Helena Su <helenasu@berkeley.edu>
2023-07-05 21:17:41 -0400

    Updates

---
+++ lib/components/Header.svelte
@@ -0,0 +1,18 @@
+<!-- Header.svelte -->
+<header>
+    <a href="/">Home</a>
+  
+    <nav>
+      <ul>
+        <li>
+          <a href="/blog">Blog</a>
+        </li>
+        <li>
+            <a href="https://helenawsu.github.io/photography/">Photography</a>
+          </li>
+        <li>
+          <a href="/about">About</a>
+        </li>
+      </ul>
+    </nav>
+  </header>
---
+++ routes/+layout.svelte
@@ -0,0 +1,16 @@
+<!-- +layout.svelte -->
+<script>
+import Header from '$lib/components/Header.svelte'
+import '$lib/styles/style.css'
+export const prerender = true;
+
+</script>
+<Header />
+
+<header>Hi, I'm a header</header>
+
+<main>
+  <slot />
+</main>
+
+<footer>Hello, I'm the footer.</footer>
